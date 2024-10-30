e05402cb2907104b4c616800a99a415af212e843
Paul Menhart <paul@menhart.net>
2024-10-30 14:32:45 +0100

    implement camera rendering

--- src/pages/5.py
+++ src/pages/5.py
@@ -6,7 +6,14 @@ import streamlit as st
 import transforms3d as tr
 from numpy.linalg import inv
 
-from utils.plot import init_figure, plot_camera, plot_points, plot_world_coordinates
+from utils.plot import (
+    SH,
+    SW,
+    init_figure,
+    plot_camera,
+    plot_points,
+    plot_world_coordinates,
+)
 
 EXERCISE = 5
 TASKS = 3
@@ -27,6 +34,9 @@ with tabs[task - 1]:
     R1 = tr.euler.euler2mat(0.5 * pi, 0, -0.25 * pi)
     T1 = [xmax, ymax, 0]
     W1 = tr.affines.compose(T1, R1, np.ones(3))
+    # R1 = tr.euler.euler2mat(pi, 0, 0)
+    # T1 = [0, 0, 5]
+    # W1 = tr.affines.compose(T1, R1, np.ones(3))
 
     R2 = tr.euler.euler2mat(0.5 * pi, 0, 0.25 * pi)
     T2 = [xmin, ymax, 0]
@@ -89,7 +99,7 @@ with tabs[task - 1]:
 
     b1 = np.array(list(map(lambda p: [p[0] / p[2], p[1] / p[2], 1], p1)))
     b2 = np.array(list(map(lambda p: [p[0] / p[2], p[1] / p[2], 1], p2)))
-    disparity = np.subtract(b2[:, 0], b1[:, 0])
+    disparity = np.multiply(np.subtract(b2[:, 0], b1[:, 0]), 2)
 
     x, y, z = p0.T
     fig1.add_trace(
@@ -101,6 +111,46 @@ with tabs[task - 1]:
             name="test",
             legendgroup="test",
             marker=dict(size=5, color=-disparity, line_width=0.0, colorscale="RdBu"),
+        )
+    )
+
+    c1_disparity = (np.array(list(zip(x, y, disparity))) - T1) @ inv(R1.T)
+    c1_disparity_projected = np.array(
+        list(map(lambda p: [p[0] / p[2], p[1] / p[2], 1], c1_disparity))
+    )
+    c1xp, c1yp, c1zp = c1_disparity_projected.T
+    mask = (-SW <= c1xp) * (c1xp <= SW) * (-SH <= c1yp) * (c1yp <= SH)
+    c1_disparity_projected_filtered = c1_disparity_projected[mask, :]
+    c1xf, c1yf, c1zf = (c1_disparity_projected_filtered @ R1.T + T1).T
+    fig1.add_trace(
+        go.Scatter3d(
+            x=c1xf,
+            y=c1yf,
+            z=c1zf,
+            mode="markers",
+            name="test",
+            legendgroup="test",
+            marker=dict(size=1, color=-c1zf, line_width=0.0, colorscale="RdBu"),
+        )
+    )
+
+    c2_disparity = (np.array(list(zip(x, y, disparity))) - T2) @ inv(R2.T)
+    c2_disparity_projected = np.array(
+        list(map(lambda p: [p[0] / p[2], p[1] / p[2], 1], c2_disparity))
+    )
+    c2xp, c2yp, c2zp = c2_disparity_projected.T
+    mask = (-SW <= c2xp) * (c2xp <= SW) * (-SH <= c2yp) * (c2yp <= SH)
+    c2_disparity_projected_filtered = c2_disparity_projected[mask, :]
+    c2xf, c2yf, c2zf = (c2_disparity_projected_filtered @ R2.T + T2).T
+    fig1.add_trace(
+        go.Scatter3d(
+            x=c2xf,
+            y=c2yf,
+            z=c2zf,
+            mode="markers",
+            name="test",
+            legendgroup="test",
+            marker=dict(size=1, color=-c2zf, line_width=0.0, colorscale="RdBu"),
         )
     )
 

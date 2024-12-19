cd48c611b76d2e19f3bfbf4aa78b6180349b07b6
Paul Menhart <paul_menhart@genua.de>
2024-12-19 18:55:53 +0100

    read camera poses in project.py

--- src/pages/8_project.py
+++ src/pages/8_project.py
@@ -7,7 +7,6 @@ import plotly.graph_objects as go
 import streamlit as st
 
 import utils
-from utils.plot import init_figure
 
 EXERCISES = 6
 TITLE = f"Project"
@@ -17,10 +16,13 @@ exercise = 5
 
 tabs = st.tabs(list(map(lambda x: str(x), range(1, EXERCISES + 1))))
 
-DATA_PATH = Path(os.getcwd() + "/../data/LV_8/")
+DATA_PATH = Path(os.getcwd() + "/../data/Proj/lab_10")
 PATHS_LEFT = sorted((DATA_PATH / "mod/left/").glob("*.jpg"))
 PATHS_RIGHT = sorted((DATA_PATH / "mod/right/").glob("*.jpg"))
 PATHS_DISPARITY = sorted((DATA_PATH / "mod/disp/").glob("*.jpg"))
+TRAJECTORY_PATH = (
+    DATA_PATH / "tsukuba_ground_truth_poses/tsukuba_ground_truth_poses.txt"
+)
 
 exercise += 1
 with tabs[exercise - 6]:
@@ -28,18 +30,9 @@ with tabs[exercise - 6]:
 
     scale = 0.3
     # read an image from disk
-    images_left = [
-        cv.resize(cv.imread(str(p)), (0, 0), fx=scale, fy=scale) for p in PATHS_LEFT
-    ]
-    images_right = [
-        cv.resize(cv.imread(str(p)), (0, 0), fx=scale, fy=scale) for p in PATHS_RIGHT
-    ]
-    disparities = [
-        cv.cvtColor(
-            cv.resize(cv.imread(str(p)), (0, 0), fx=scale, fy=scale), cv.COLOR_BGR2GRAY
-        )
-        for p in PATHS_DISPARITY
-    ]
+    images_left = [utils.load_image(p, scale=scale) for p in PATHS_LEFT]
+    images_right = [utils.load_image(p, scale=scale) for p in PATHS_RIGHT]
+    disparities = [utils.load_image(p, scale=scale, gray=True) for p in PATHS_DISPARITY]
 
     ca1, ca2 = [], []
     columns = st.columns(2)
@@ -47,13 +40,15 @@ with tabs[exercise - 6]:
     ca2.append(columns[1])
     ca1[0].write("Matching left side images:")
     ca2[0].write("Matching right side images:")
-    start_image = images_left.pop(0)
-    for idx, disparity in enumerate(images_left):
-        matches, keypoints1, keypoints2 = utils.surf_match(start_image, disparity)
+    start_image_left = images_left[0]
+    matches_left = []
+    for idx, disparity in enumerate(images_left[1:]):
+        matches, keypoints1, keypoints2 = utils.surf_match(start_image_left, disparity)
+        matches_left.append(matches)
 
         # cv.drawMatchesKnn expects list of lists as matches.
         image_matches = cv.drawMatchesKnn(
-            start_image,
+            start_image_left,
             keypoints1,
             disparity,
             keypoints2,
@@ -68,21 +63,23 @@ with tabs[exercise - 6]:
         ca1[idx + 1].image(image_matches)
         ca1[idx + 1].code(f"{len(matches)=}")
 
-    start_image = images_right.pop(0)
-    for idx, disparity in enumerate(images_right):
-        matches, keypoints1, keypoints2 = utils.surf_match(start_image, disparity)
+    start_image_right = images_right[0]
+    matches_right = []
+    for idx, disparity in enumerate(images_right[1:]):
+        matches, keypoints1, keypoints2 = utils.surf_match(start_image_right, disparity)
+        matches_right.append(matches)
         matched_images = utils.draw_matches(
-            start_image, keypoints1, disparity, keypoints2, matches
+            start_image_right, keypoints1, disparity, keypoints2, matches
         )
         columns = st.columns(2)
         ca2[idx + 1].image(matched_images)
         ca2[idx + 1].code(f"{len(matches)=}")
 
-
 exercise += 1
 with tabs[exercise - 6]:
     st.write(f"## 7.2.{exercise}.1")
     cb1, cb2 = [], []
+    CAMERA_INTRINSICS = [[615, 0, 320], [0, 615, 240], [0, 0, 1]]
     for idx, disparity in enumerate(disparities):
         image = images_left[idx]
 
@@ -93,6 +90,7 @@ with tabs[exercise - 6]:
         cb1[idx].image(image)
         cb2[idx].image(disparity)
         x, y, z = utils.disparity_to_points(disparity)
+
         width, height, _ = image.shape
         colors = []
         for w in range(width):
@@ -101,7 +99,7 @@ with tabs[exercise - 6]:
                     f'rgb{np.array2string(image[w][h], separator=",").replace("[", "(").replace("]", ")")}'
                 )
 
-        point_cloud = init_figure(
+        point_cloud = utils.init_figure(
             eye={"x": 0, "y": 0, "z": 1.0}, up={"x": 1, "y": 0, "z": 0}
         )
         points_reconstructed = np.stack((x.ravel(), y.ravel(), z.ravel()), axis=1)
@@ -124,3 +122,7 @@ with tabs[exercise - 6]:
                 )
             )
             st.plotly_chart(point_cloud)
+        break
+
+    camera_poses = utils.read_poses(TRAJECTORY_PATH)
+    st.video(utils.image_to_video(images_left))

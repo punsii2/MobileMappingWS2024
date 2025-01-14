bf990091dc63b476feee06648432249553f3b2a0
Paul Menhart <paul@menhart.net>
2025-01-14 23:56:20 +0100

    implement ransac+pose estimation

--- src/pages/8_project.py
+++ src/pages/8_project.py
@@ -7,16 +7,16 @@ import plotly.graph_objects as go
 import streamlit as st
 
 import utils
+from utils.plot import init_figure, plot_camera
 
 EXERCISES = 6
-TITLE = f"Project"
+TITLE = "Project"
 st.set_page_config(page_title=TITLE, page_icon="🗺️", layout="wide")
 st.sidebar.header(TITLE)
-exercise = 5
 
 tabs = st.tabs(list(map(lambda x: str(x), range(1, EXERCISES + 1))))
 
-DATA_PATH = Path(os.getcwd() + "/../data/Proj/lab_10")
+DATA_PATH = Path(os.getcwd() + "/../data/Proj/lab40")
 PATHS_LEFT = sorted((DATA_PATH / "mod/left/").glob("*.jpg"))
 PATHS_RIGHT = sorted((DATA_PATH / "mod/right/").glob("*.jpg"))
 PATHS_DISPARITY = sorted((DATA_PATH / "mod/disp/").glob("*.jpg"))
@@ -24,9 +24,10 @@ TRAJECTORY_PATH = (
     DATA_PATH / "tsukuba_ground_truth_poses/tsukuba_ground_truth_poses.txt"
 )
 
-exercise += 1
-with tabs[exercise - 6]:
+exercise = 1
+with tabs[exercise - 1]:
     st.write(f"## 7.2.{exercise}.1")
+    st.write("### Extract and Match Features")
 
     scale = 0.3
     # read an image from disk
@@ -41,17 +42,26 @@ with tabs[exercise - 6]:
     ca1[0].write("Matching left side images:")
     ca2[0].write("Matching right side images:")
     start_image_left = images_left[0]
-    matches_left = []
-    for idx, disparity in enumerate(images_left[1:]):
-        matches, keypoints1, keypoints2 = utils.surf_match(start_image_left, disparity)
-        matches_left.append(matches)
+    keypoint_matches_left_origin = []
+    keypoint_matches_left_target = []
+
+    for idx, image_left in enumerate(images_left[1:]):
+        matches, keypoints_origin, keypoints_target = utils.surf_match(
+            start_image_left, image_left
+        )
+        keypoint_matches_left_origin.append(
+            np.array([keypoints_origin[match[0].queryIdx].pt for match in matches])
+        )
+        keypoint_matches_left_target.append(
+            np.array([keypoints_target[match[0].trainIdx].pt for match in matches])
+        )
 
         # cv.drawMatchesKnn expects list of lists as matches.
         image_matches = cv.drawMatchesKnn(
             start_image_left,
-            keypoints1,
-            disparity,
-            keypoints2,
+            keypoints_origin,
+            image_left,
+            keypoints_target,
             matches,
             None,
             flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
@@ -64,20 +74,38 @@ with tabs[exercise - 6]:
         ca1[idx + 1].code(f"{len(matches)=}")
 
     start_image_right = images_right[0]
-    matches_right = []
-    for idx, disparity in enumerate(images_right[1:]):
-        matches, keypoints1, keypoints2 = utils.surf_match(start_image_right, disparity)
-        matches_right.append(matches)
-        matched_images = utils.draw_matches(
-            start_image_right, keypoints1, disparity, keypoints2, matches
+    keypoint_matches_right_origin = []
+    keypoint_matches_right_target = []
+
+    for idx, image_right in enumerate(images_right[1:]):
+        matches, keypoints_origin, keypoints_target = utils.surf_match(
+            start_image_right, image_right
         )
-        columns = st.columns(2)
-        ca2[idx + 1].image(matched_images)
+        keypoint_matches_right_origin.append(
+            np.array([keypoints_origin[match[0].queryIdx].pt for match in matches])
+        )
+        keypoint_matches_right_target.append(
+            np.array([keypoints_target[match[0].trainIdx].pt for match in matches])
+        )
+
+        # cv.drawMatchesKnn expects list of lists as matches.
+        image_matches = cv.drawMatchesKnn(
+            start_image_right,
+            keypoints_origin,
+            image_right,
+            keypoints_target,
+            matches,
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+        )
+        ca2[idx + 1].image(image_matches)
         ca2[idx + 1].code(f"{len(matches)=}")
 
-exercise += 1
-with tabs[exercise - 6]:
+exercise = 2
+with tabs[exercise - 1]:
     st.write(f"## 7.2.{exercise}.1")
+    st.write("### Dense Matching")
+
     cb1, cb2 = [], []
     CAMERA_INTRINSICS = [[615, 0, 320], [0, 615, 240], [0, 0, 1]]
     for idx, disparity in enumerate(disparities):
@@ -89,15 +117,9 @@ with tabs[exercise - 6]:
 
         cb1[idx].image(image)
         cb2[idx].image(disparity)
-        x, y, z = utils.disparity_to_points(disparity)
 
-        width, height, _ = image.shape
-        colors = []
-        for w in range(width):
-            for h in range(height):
-                colors.append(
-                    f'rgb{np.array2string(image[w][h], separator=",").replace("[", "(").replace("]", ")")}'
-                )
+        x, y, z = utils.disparity_to_points(disparity)
+        colors = utils.image_to_rgb_strings(image)
 
         point_cloud = utils.init_figure(
             eye={"x": 0, "y": 0, "z": 1.0}, up={"x": 1, "y": 0, "z": 0}
@@ -124,5 +146,157 @@ with tabs[exercise - 6]:
             st.plotly_chart(point_cloud)
         break
 
-    camera_poses = utils.read_poses(TRAJECTORY_PATH)
     st.video(utils.image_to_video(images_left))
+
+    camera_poses = utils.read_poses(TRAJECTORY_PATH)[: len(PATHS_LEFT)]
+
+    points_sequence = []
+    colors_sequence = []
+    for idx, disparity in enumerate(disparities):
+        colors_sequence.extend(utils.image_to_rgb_strings(images_left[idx]))
+
+        x, y, z = utils.disparity_to_points(disparity)
+
+        coordinates = (x, y, z, np.zeros(len(x)))
+        normalized_points = np.concatenate(coordinates).reshape((-1, 4), order="F")
+        world_points = normalized_points @ camera_poses[idx].T
+        points_sequence.extend(world_points)
+
+    point_cloud = utils.init_figure(
+        eye={"x": 0, "y": 0, "z": 1.0}, up={"x": 1, "y": 0, "z": 0}
+    )
+    x = [p[0] for p in points_sequence]
+    y = [p[1] for p in points_sequence]
+    z = [p[2] for p in points_sequence]
+    point_cloud.add_trace(
+        go.Scatter3d(
+            x=x,
+            y=y,
+            z=z,
+            mode="markers",
+            name="test",
+            legendgroup="test",
+            marker=dict(
+                size=1,
+                color=colors_sequence,
+                line_width=0.0,
+            ),
+        )
+    )
+    st.plotly_chart(point_cloud)
+
+exercise = 3
+with tabs[exercise - 1]:
+    st.write(f"## 7.2.{exercise}.1")
+    st.write("### Ransac und E-Matrix berechnen")
+
+    st.write(
+        "Rang muss gleich 2 sein => Ein Eigenwert ist 0 (bzw. sehr, sehr klein) und 2 ungleich 0."
+    )
+    st.write("Linke Kamera:")
+
+    # Find fundamental matrix and filter keypoints for the left images
+    rotations_left = []
+    translations_left = []
+    for i in range(len(keypoint_matches_left_origin)):
+        keypoints_origin = keypoint_matches_left_origin[i]
+        keypoints_target = keypoint_matches_left_target[i]
+        F, FMask = cv.findFundamentalMat(
+            keypoints_origin,
+            keypoints_target,
+        )
+        E, EMask = cv.findEssentialMat(
+            keypoints_origin,
+            keypoints_target,
+        )
+
+        valid_keypoints_origin = np.array([e[0] for e in keypoints_origin[EMask]])
+        valid_keypoints_target = np.array([e[0] for e in keypoints_target[EMask]])
+        st.code(f"{np.linalg.matrix_rank(F)=}")
+        st.code(f"{np.linalg.eigvals(F)=}")
+        st.code(f"{np.linalg.matrix_rank(E)=}")
+        st.code(f"{np.linalg.eigvals(E)=}")
+
+        _, R, t, _ = cv.recoverPose(
+            E,
+            valid_keypoints_origin,
+            valid_keypoints_target,
+            np.array(CAMERA_INTRINSICS),
+        )
+        rotations_left.append(R)
+        translations_left.append([tr[0] for tr in t])
+        columns = st.columns(2)
+        columns[0].write("F")
+        columns[0].write(F)
+        columns[1].write("E")
+        columns[1].write(E)
+        st.divider()
+
+    st.write("Rechte Kamera:")
+
+    # Find fundamental matrix and filter keypoints for the left images
+    rotations_right = []
+    translations_right = []
+    for i in range(len(keypoint_matches_right_origin)):
+        keypoints_origin = keypoint_matches_right_origin[i]
+        keypoints_target = keypoint_matches_right_target[i]
+        F, FMask = cv.findFundamentalMat(
+            keypoints_origin,
+            keypoints_target,
+        )
+        E, EMask = cv.findEssentialMat(
+            keypoints_origin,
+            keypoints_target,
+        )
+
+        valid_keypoints_origin = np.array([e[0] for e in keypoints_origin[EMask]])
+        valid_keypoints_target = np.array([e[0] for e in keypoints_target[EMask]])
+        _, R, t, _ = cv.recoverPose(
+            E,
+            valid_keypoints_origin,
+            valid_keypoints_target,
+            np.array(CAMERA_INTRINSICS),
+        )
+        rotations_right.append(R)
+        translations_right.append([tr[0] for tr in t])
+        columns = st.columns(2)
+        columns[0].write("F")
+        columns[0].write(F)
+        columns[1].write("E")
+        columns[1].write(E)
+        st.divider()
+
+exercise = 4
+with tabs[exercise - 1]:
+    st.write(f"## 7.2.{exercise}.1")
+    st.write("### Posen berechnen ")
+
+    fig = init_figure()
+
+    for i in range(len(rotations_left)):
+        R = rotations_left[i]
+        t = translations_left[i]
+        W = np.array(
+            [
+                [R[0, 0], R[0, 1], R[0, 2], t[0]],
+                [R[1, 0], R[1, 1], R[1, 2], t[1]],
+                [R[2, 0], R[2, 1], R[2, 2], t[2]],
+                [0, 0, 0, 1],
+            ]
+        )
+        plot_camera(fig, W, name="Left Camera", color="blue")
+
+    for i in range(len(rotations_right)):
+        R = rotations_right[i]
+        t = translations_right[i]
+        W = np.array(
+            [
+                [R[0, 0], R[0, 1], R[0, 2], t[0]],
+                [R[1, 0], R[1, 1], R[1, 2], t[1]],
+                [R[2, 0], R[2, 1], R[2, 2], t[2]],
+                [0, 0, 0, 1],
+            ]
+        )
+        plot_camera(fig, W, name="Right Camera", color="red")
+
+    st.plotly_chart(fig)
--- src/utils/image.py
+++ src/utils/image.py
@@ -1,4 +1,3 @@
-import tempfile
 import uuid
 from pathlib import Path
 from typing import List, Optional
@@ -12,13 +11,15 @@ import streamlit as st
 def load_image(path: Path, scale: Optional[float], gray: bool = False) -> np.ndarray:
     image = cv.imread(str(path))
     if scale and scale != 1.0:
-        image = cv.resize(image, (0, 0), fx=scale, fy=scale)
+        image = cv.cvtColor(
+            cv.resize(image, (0, 0), fx=scale, fy=scale), cv.COLOR_BGR2RGB
+        )
     if gray:
         image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     return image
 
 
-@st.cache_data(persist=True)
+# @st.cache_data(persist=True)
 def image_to_video(images):
     height = images[0].shape[0]
     width = images[0].shape[1]
@@ -37,7 +38,7 @@ def image_to_video(images):
     return video
 
 
-@st.cache_data(persist=True)
+@st.cache_data
 def read_poses(path: Path) -> List[np.ndarray]:
     poses = []
     with path.open() as f:
@@ -48,3 +49,15 @@ def read_poses(path: Path) -> List[np.ndarray]:
                 ).reshape([4, 4])
             )
     return poses
+
+
+def image_to_rgb_strings(image: np.ndarray) -> List[str]:
+    colors = []
+    width, height, _ = image.shape
+    for w in range(width):
+        for h in range(height):
+            colors.append(
+                f'rgb{np.array2string(image[w][h], separator=",").replace("[", "(").replace("]", ")")}'
+            )
+
+    return colors
